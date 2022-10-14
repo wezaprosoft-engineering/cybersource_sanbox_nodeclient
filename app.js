2c883136e290f76b339ef6b6676fde211a9c4091
Stephen Gachoka <stephengachoka57@gmail.com>
2022-10-14 13:08:17 +0300

    cors

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ const express = require('express');
 const createError = require('http-errors');
 const morgan = require('morgan');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors())
+
 
 app.set('view engine', 'ejs');
 
