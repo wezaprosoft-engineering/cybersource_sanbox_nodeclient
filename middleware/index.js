24a2ae43c2b99fd4fed3339e533e92258fc3b536
Stephen Gachoka <stephengachoka57@gmail.com>
2022-10-14 13:24:33 +0300

    rm setheader

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,5 @@
 const verifyPayload = async (req, res, next) => {
   try {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     console.log("Body ====>", req.body);
     if (req.body === {}) {
       res.send({ message: "Body can't be empty" });
