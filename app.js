584ebcab40faf327b7999a23a95e52cb5348f721
Stephen Gachoka <stephengachoka57@gmail.com>
2022-10-14 12:31:04 +0300

    cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use(cors())
+app.use(cors({credentials: true, origin: true}))
 
 app.set('view engine', 'ejs');
 
